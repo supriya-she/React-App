afe9ca70e025899c9fd96764c8ca2e6f8dfbb88f
supriya-she <133473602+supriya-she@users.noreply.github.com>
2023-12-12 08:46:00 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+import TripCalculator from './TripCalculator';
+
+function App() {
+  return (
+    <div className="App">
+      <TripCalculator />
+    </div>
+  );
+}
+
+export default App;
